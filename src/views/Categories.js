db18953e9614d5a9cc0318f2c98a6328d4d92c37
Josse2002 <jcastillo10b@gmail.com>
2023-02-04 18:43:06 -0600

    :star: categorias

--- src/views/Categories.js
+++ src/views/Categories.js
@@ -2,12 +2,14 @@ import React from "react";
 import "../App.css"
 import Header from "../components/Header";
 import Categorie_banner from "../components/Categorie_banner";
+import Productcategorie from "../components/product-categorie";
 
 function Categories({categoria}) {
     return (
         <div className="categorias">
             <Header />
             <Categorie_banner categoriaBanner={categoria}/>
+            <Productcategorie categoria={categoria}/>
         </div>
     );
 }
